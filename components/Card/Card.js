fdcb82dc9fb5995b0289226dac40a4bdcb0497e0
Ezzio Salas <ezzio.salas@gensuitellc.com>
2021-11-25 13:28:52 -0600

    Whats new? -Custom Border and Card Color

--- components/Card/Card.js
+++ components/Card/Card.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-export default function Card({ children, className,background }) {
+export default function Card({ children, className,background='bg-white',round='xl',padding='p-4',hover=false }) {
     return (
         <div
-            className={`w-full ${background} rounded-xl overflow-hdden shadow-md p-4 ${className}`}
+            className={`w-full ${background} rounded-${round} overflow-hdden shadow-md ${padding} ${className}  ${hover?'hover:bg-gray-200 cursor-pointer':''}`}
         >
             {children}
         </div>
